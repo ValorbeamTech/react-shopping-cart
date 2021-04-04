2e9263ca30bea04bbaed84c8f0ff6f7d40994222
sfingers <valorbeam.info@gmail.com>
2021-04-04 11:16:09 -0700

    initial comit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        Footer Content
+      </footer>
+    </div>
+  );
+}
+
+export default App;
