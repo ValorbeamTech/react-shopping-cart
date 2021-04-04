95d39dcdbc72d1ff388521cbb04c07a0d7e35a1b
sfingers <valorbeam.info@gmail.com>
2021-04-04 12:54:23 -0700

    product-components-create

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+import React from "react"
+import Product from "./components/Product"
+import data from "./data.json"
 
+class App extends React.Component{
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">Shopping Cart</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        Footer Content
-      </footer>
-    </div>
-  );
+  constructor(){
+    super()
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    }
+  }
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Product products={this.state.products}></Product>
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>
+          Footer Content
+        </footer>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
